68ae353ebaec8a9517918b25714a888daa32bb26
Hvquys <hvquys@gmail.com>
2024-03-24 21:56:55 +0700

    Thêm các tập tin mới và thực hiện các sửa đổi

---
+++ NewLibs/CheckValueInput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewLibs
+{
+    public class CheckValueInput
+    {
+        public static bool CheckIsInt(string inputValue)
+        {
+            int result;
+            return int.TryParse(inputValue, out result);
+        }
+
+        public static bool CheckIsFloat(string inputValue)
+        {
+            float result;
+            return float.TryParse(inputValue, out result);
+        }
+    }
+}
